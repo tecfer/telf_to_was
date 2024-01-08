014035b11facfc86f881735dd7eb884b550d6566
Carlos F.O. <tecfer2020@gmail.com>
2024-01-08 21:52:34 +0100

    Corrección de errores

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "es.tecfer.telftowas"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/es/tecfer/telftowas/MainActivity.kt
+++ app/src/main/java/es/tecfer/telftowas/MainActivity.kt
@@ -6,34 +6,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val phoneNumberEditText: EditText = findViewById(R.id.phoneNumberEditText)
+        val et_phoneNumber: EditText = findViewById(R.id.et_phoneNumber)
         val submitButton: Button = findViewById(R.id.submitButton)
 
         submitButton.setOnClickListener {
-            val phoneNumber = phoneNumberEditText.text.toString().trim()
+            val phoneNumber = et_phoneNumber.text.toString().trim()
 
             if (phoneNumber.isNotEmpty()) {
-                // Construir el enlace web con el contenido del EditText
-                val webUrl = "https://wa.me/$phoneNumber"
 
-                // Crear un Intent para abrir el enlace web
+                val webUrl = "https://api.whatsapp.com/send/?phone=$phoneNumber"
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(webUrl))
 
-                // Verificar si hay aplicaciones que pueden manejar este intent
-                if (intent.resolveActivity(packageManager) != null) {
-                    // Si hay una aplicación, iniciar la actividad
-                    startActivity(intent)
-                } else {
-                    // Manejar el caso en el que no haya aplicaciones para manejar el intent
-                    // Puedes mostrar un mensaje o realizar alguna otra acción
-                }
+                startActivity(intent)
             }
         }
+    }
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
